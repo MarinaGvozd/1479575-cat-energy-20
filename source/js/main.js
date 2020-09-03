978c901bb3862b6069a17b9b8312471b96bda35a
MarinaGvozd <65240984+MarinaGvozd@users.noreply.github.com>
2020-09-04 00:08:29 +0300

    добавила слайдер

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,9 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav')
+, navToggle = document.querySelector('.main-nav__toggle')
+, slider = document.querySelector(".slider")
+, buttonBefore = document.querySelector(".slider__toggle--before")
+, buttonAfter = document.querySelector(".slider__toggle--after")
+, sliderIndicator = document.querySelector(".slider__indicator");
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
@@ -9,4 +13,20 @@ navToggle.addEventListener('click', function() {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
+});
+
+
+buttonBefore.addEventListener("click", function() {
+    console.log('slider', slider);
+    slider.classList.contains("slider--after") && (slider.classList.remove("slider--after"),
+    slider.classList.add("slider--before"))
+});
+buttonAfter.addEventListener("click", function() {
+    slider.classList.contains("slider--before") && (slider.classList.remove("slider--before"),
+    slider.classList.add("slider--after"))
+});
+sliderIndicator.addEventListener("click", function() {
+    slider.classList.contains("slider--before") ? (slider.classList.remove("slider--before"),
+    slider.classList.add("slider--after")) : (slider.classList.remove("slider--after"),
+    slider.classList.add("slider--before"))
 });
