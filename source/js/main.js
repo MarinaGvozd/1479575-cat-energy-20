f283e7243f8251f19385dd966d32f92f451ba419
MarinaGvozd <65240984+MarinaGvozd@users.noreply.github.com>
2020-09-08 19:12:31 +0300

    finushed fix

--- source/js/main.js
+++ source/js/main.js
@@ -1,30 +1,30 @@
-var navMain = document.querySelector('.main-nav')
-, navToggle = document.querySelector('.main-nav__toggle')
-, slider = document.querySelector(".slider")
+// var navMain = document.querySelector('.main-nav')
+// , navToggle = document.querySelector('.main-nav__toggle')
+var slider = document.querySelector(".slider")
 , buttonBefore = document.querySelector(".slider__toggle--before")
 , buttonAfter = document.querySelector(".slider__toggle--after")
 , sliderIndicator = document.querySelector(".slider__indicator");
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
+// navToggle.addEventListener('click', function() {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--closed');
+//     navMain.classList.remove('main-nav--opened');
+//   }
+// });
 
 
-buttonBefore.addEventListener("click", function() {
+buttonBefore && buttonBefore.addEventListener("click", function() {
     slider.classList.contains("slider--after") && (slider.classList.remove("slider--after"),
     slider.classList.add("slider--before"))
 });
-buttonAfter.addEventListener("click", function() {
+buttonAfter && buttonAfter.addEventListener("click", function() {
     slider.classList.contains("slider--before") && (slider.classList.remove("slider--before"),
     slider.classList.add("slider--after"))
 });
-sliderIndicator.addEventListener("click", function() {
+sliderIndicator && sliderIndicator.addEventListener("click", function() {
     slider.classList.contains("slider--before") ? (slider.classList.remove("slider--before"),
     slider.classList.add("slider--after")) : (slider.classList.remove("slider--after"),
     slider.classList.add("slider--before"))
